91835d0d3d84eab053102929c0aee0ba75a50238
Jory Roberts <robertsjory13@gmail.com>
2023-03-26 08:36:20 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const superbowlWin = (record) => {
+  
+  const objectWithAWin = record.find((element) => element.result === "W");
+
+  return objectWithAWin ? objectWithAWin.year : undefined
+
+  }
+
+
+
+
+ 
